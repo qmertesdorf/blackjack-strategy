c1e9a13a01bbd80e487fbbd0018f2d97fa1878c9
qmertesdorf <quintenmertesdorf@gmail.com>
2019-05-21 15:06:28 -0700

    add SASS; add initial Card component/design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "./Card";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <Card />
       </header>
     </div>
   );
---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card.scss';
+
+const Card = ({suit, value}) => {
+  return (
+  <div className="card">
+    
+  </div>
+  )
+}
+
+export default Card;
