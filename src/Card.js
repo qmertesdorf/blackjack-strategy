adfd02732110a11620a7a04e9ec0bc682fe09d0f
qmertesdorf <quintenmertesdorf@gmail.com>
2019-05-21 16:35:44 -0700

    add card constant; add intiial style for card

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './card.scss';
+import { SUITS } from './constants';
+import React from "react";
+import "./card.scss";
 
-const Card = ({suit, value}) => {
+const Card = ({ suitId, value }) => {
+  const suitIcon = getIconBySuitId(suitId)
   return (
-  <div className="card">
-    
-  </div>
-  )
-}
+    <div className="card">
+      <p className="card-value top-left">10</p>
+      <div className="suit-icon-container">
+        <i className="suit-icon">{suitIcon}</i>
+      </div>
+      <p className="card-value bottom-right">10</p>
+    </div>
+  );
+};
+
+function getIconBySuitId(id) {
+  if (id === SUITS.CLUBS) {
+    return "♣";
+  };
+  if (id === SUITS.DIAMONDS) {
+    return "♢";
+  };
+  if (id === SUITS.HEARTS) {
+    return "♡";
+  };
+  if (id === SUITS.SPADES) {
+    return "♠";
+  };
+};
 
 export default Card;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const SUITS = {
+  CLUBS: 0,
+  DIAMONDS: 1,
+  HEARTS: 2,
+  SPADES: 3
+}
