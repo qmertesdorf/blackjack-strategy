66da3d75d1d35dd108c25a9e19a98fe967b75d5c
unknown <quintanius@gmail.com>
2019-05-24 08:25:51 -0700

    add dealer card generation; add question set generation

--- src/PlayerHand.js
+++ src/PlayerHand.js
@@ -1,4 +1,4 @@
-import { SUITS, values } from "./constants";
+import { SUITS, VALUES, SPLITTING, CHOICES } from "./constants";
 import React from "react";
 import "./playerHand.scss";
 import Card from "./Card";
@@ -6,11 +6,26 @@ import Card from "./Card";
 const PlayerHand = () => {
   const cardOneData = generateCardData();
   const cardTwoData = generateCardData();
-  
+  const dealerCard = generateCardData();
+  const questionSet = determineQuestionSetFromCardData();
+
+  const correctAnswer = determineCorrectAnswer(cardOneData, cardTwoData, dealerCard, questionSet);
+  // const questionAnswer;
+
   return (
     <div className="player-hand">
-      <Card {...cardOneData} />
-      <Card {...cardTwoData} />
+      <Card
+        {...{
+          suitId: cardOneData.suitId,
+          value: cardOneData.value.cardValKey
+        }}
+      />
+      <Card
+        {...{
+          suitId: cardTwoData.suitId,
+          value: cardTwoData.value.cardValKey
+        }}
+      />
     </div>
   );
 };
@@ -19,19 +34,32 @@ function generateCardData() {
   const suitId = Object.values(SUITS)[
     Math.floor(Math.random() * Object.values(SUITS).length)
   ];
-  const value = Object.values(values)[
-    Math.floor(Math.random() * Object.values(values).length)
+  const cardValKey = Object.keys(VALUES)[
+    Math.floor(Math.random() * Object.keys(VALUES).length)
   ];
+  const cardValValue = VALUES[cardValKey];
   return {
     suitId: suitId,
-    value: value
+    value: { cardValKey: cardValValue }
   };
 }
 
-function getCorrectAnswerFromCardData(cardOne, cardTwo) {
-  const cardOneValue = cardOne.value;
-  const cardTwoValue = cardTwo.value;
+function determineQuestionSetFromCardData(cardOne, cardTwo) {
+  const cardOneValue = cardOne.value.cardValValue;
+  const cardTwoValue = cardTwo.value.cardValValue;
 
+  if (cardOneValue === cardTwoValue) {
+    return SPLITTING;
+  } else {
+    return CHOICES;
+  }
+
+}
+
+function determineCorrectAnswer(cardOne, cardTwo, dealerCard, questionSet) {
+  if (questionSet === SPLITTING) {
+    
+  }
 }
 
 export default PlayerHand;
--- src/constants.js
+++ src/constants.js
@@ -5,21 +5,38 @@ export const SUITS = {
   SPADES: 3
 }
 
-export const values = {
-  ace: "A",
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-  ten: 10,
-  jack: "J",
-  queen: "Q",
-  king: "K"
+// export const values = {
+//   ace: "A",
+//   one: 1,
+//   two: 2,
+//   three: 3,
+//   four: 4,
+//   five: 5,
+//   six: 6,
+//   seven: 7,
+//   eight: 8,
+//   nine: 9,
+//   ten: 10,
+//   jack: "J",
+//   queen: "Q",
+//   king: "K"
+// }
+
+export const VALUES = {
+  A: "Ace",
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 10,
+  Q: 10,
+  K: 10
 }
 
 export const CHOICES = {
